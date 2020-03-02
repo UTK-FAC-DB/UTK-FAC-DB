484f2c19ad6e98e4d78ad1bf483405aab2c3cd93
jakewebbdev <jakewebbdev@gmail.com>
2020-03-01 18:07:43 -0800

    implement all API endpoints and create angular web app

--- db-app/src/app/app-routing.module.ts
+++ db-app/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DonorListComponent } from './donors/donor-list/donor-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DonorListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
