66391f6772cba3372ea145a50719921b4c642f2f
Chase Brunette-Pak <chasebrunettepak@gmail.com>
2020-04-18 13:19:24 -0400

    #3 implemented new server side authen for later token usages as well as resturctured

---
+++ db-app/api/models/user.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+/* Will eventually need to add priveleges here... */
+
+var userSchema = mongoose.Schema({
+    firstName: { type: String, required: true},
+    lastName: { type: String, required: true},
+    userName: { type: String, required: true},
+    password: { type: String, required: true},
+    userRole: { type: String, required: true}
+});
+
+userSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+  };
+  
+  userSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+    return this.hash === hash;
+  };
+  
+  userSchema.methods.generateJwt = function() {
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7); // Change date eventually
+  
+    return jwt.sign({
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+      exp: parseInt(expiry.getTime() / 1000),
+    }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+  };
+
+module.exports = mongoose.model('User', userSchema);
--- db-app/server.js
+++ db-app/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
-const userRoutes = require('./routes/users');
-const donorsRoutes = require('./routes/donors');
+// Bring in user schema
+require('./api/models/user');
 
-const Donor = require('./models/donor');
+// [SH] Bring in the Passport config after model is defined
+require('./api/config/passport');
+
+// [SH] Bring in the routes for the API (delete the default routes)
+var routesApi = require('./api/routes/index');
+
+const userRoutes = require('./api/routes/users');
+const donorsRoutes = require('./api/routes/donors');
+
+const Donor = require('./api/models/donor');
 
 const app = express();
 var distDir = __dirname + "/dist/";
@@ -29,6 +39,50 @@ try {
     console.log(error);
     process.exit(1);
 }
+
+// Initialize passport and forward routes
+app.use(passport.initialize());
+app.use('/api', routesApi);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// [SH] Catch unauthorised errors
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message" : err.name + ": " + err.message});
+  }
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/donors', donorsRoutes);
