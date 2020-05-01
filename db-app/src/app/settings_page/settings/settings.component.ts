dc5f3cc12aa051391000016308c2b59c1be7edd3
Chase Brunette-Pak <chasebrunettepak@gmail.com>
2020-05-01 02:52:17 -0400

    #17 Set up settings html and ts to be prepped for user validation table

--- db-app/src/app/settings_page/settings/settings.component.ts
+++ db-app/src/app/settings_page/settings/settings.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { UserDetails, TokenPayload, AuthenticationService } from '../../authentication/authentication.service';
-import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Token } from '@angular/compiler/src/ml_parser/lexer';
+import { UserDetails, AuthenticationService } from '../../authentication/authentication.service';
 
 /* 
 Got a lot of this example from the 'Table with expandable rows'
@@ -24,20 +21,9 @@ example at https://material.angular.io/components/table/examples
 })
 export class SettingsComponent implements OnInit {
   users: UserDetails[];
-  public registerationForm: FormGroup;
-
-  // User data token
-  registerData: TokenPayload = {
-    userName: '',
-    userRole: 'graduate',
-    firstName: '',
-    lastName: '',
-    password: ''
-  };
 
   constructor(
-    private auth: AuthenticationService,
-    private formBuilder: FormBuilder
+    private auth: AuthenticationService
   ) { }
 
   // On init will need to grab the user collection from the db
@@ -51,83 +37,11 @@ export class SettingsComponent implements OnInit {
     }, (err) => {
       console.error(err);
     });
-
-    // Form inputs and validators
-    this.registerationForm = this.formBuilder.group({
-
-      // First name check
-      firstName: new FormControl('', [
-        Validators.required,
-        Validators.pattern('[A-Za-z-]*'),
-      ]),
-
-      // Last name check
-      lastName: new FormControl('', [
-        Validators.required,
-        Validators.pattern('[A-Za-z-]*'),
-      ]),
-
-      // User name check
-      userName: new FormControl('', [
-        Validators.required,
-        Validators.pattern('[A-Za-z1-9]*'),
-      ])
-    });
-
-
   }
 
-  // Column headers
-  columnsToDisplay = ['firstName', 'lastName', 'userName', 'userRole'];
+  // Column headers for valid users
+  validUsersColumns = ['firstName', 'lastName', 'userName', 'userRole'];
 
   // Whether is expanded or not
   expandedElement: UserDetails | null;
-
-  // Saving user to db
-  onSave(user: UserDetails): void {
-    var token: TokenPayload = {
-      firstName: user.firstName,
-      lastName: user.lastName,
-      userName: user.userName,
-      _id: user._id,
-    };
-
-    console.log("SAVING " + user.userName);
-    console.log(user);
-    console.log(token);
-
-    // Update user in backend
-    this.auth.updateUser(token).subscribe(() => {
-      //window.location.reload();
-    }, (err) => {
-      console.error(err);
-    });
-  }
-
-  // Changing password
-  onChangePassword(user: UserDetails): void {
-    console.log("CHANGING " + user.userName);
-
-    // Execute pop up
-  }
-
-  // Delete user from db
-  onDelete(user: UserDetails): void {
-    var token: TokenPayload;
-
-    console.log("DELETING " + user.userName);
-
-    // Set up new token data
-    token.firstName = user.firstName;
-    token.lastName = user.lastName;
-    token.userName = user.userName;
-    token.password = user.password;
-
-    // Update user in backend
-    this.auth.deleteUser(token).subscribe(() => {
-      window.location.reload();
-    }, (err) => {
-      console.error(err);
-    });
-  }
 }
